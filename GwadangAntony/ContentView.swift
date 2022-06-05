d40a01a5b0fee38cfe1fcdccbc183b6e182e016d
jsh9611 <jangsh9611@naver.com>
2022-06-05 17:36:29 +0900

    initial commit: Add project

---
+++ GwadangAntony/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GwadangAntony
+//
+//  Created by SeongHoon Jang on 2022/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GwadangAntony/GwadangAntonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GwadangAntonyApp.swift
+//  GwadangAntony
+//
+//  Created by SeongHoon Jang on 2022/06/05.
+//
+
+import SwiftUI
+
+@main
+struct GwadangAntonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
