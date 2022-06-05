e0c1035c71b95718ad7f9c335a57673ed5b5786f
jsh9611 <jangsh9611@naver.com>
2022-06-05 19:41:31 +0900

    feat: Add MainPageView

--- GwadangAntony/MainPageView.swift
+++ GwadangAntony/MainPageView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct MainPageView: View {
+    @AppStorage(StorageKeys.sugar.rawValue) private var sugar : Double = UserDefaults.standard.double(forKey: "sugar")
+    @AppStorage(StorageKeys.todaySugar.rawValue) private var todaySugar : Double = UserDefaults.standard.double(forKey: "todaySugar")
     
     @Binding var isFirstLaunching: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: SettingsView(isFirstLaunching: $isFirstLaunching)) { Text("세팅으로") }
+                        .padding()
+                }
+                Spacer()
+                Text("오늘 먹은 설탕은 \(todaySugar)입니다.")
+                Text("세팅힌 설탕 값은 \(sugar)입니다.")
+                Text("엄마 보고 싶다😭😭")
+                Spacer()
+            }
+        }
+        
     }
 }
